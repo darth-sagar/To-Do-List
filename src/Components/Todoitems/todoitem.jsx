13439ba0e6d5efda020d8658d0814ae4e4e25a31
billaMULLA1@ <work.sagarsaini@gmail.com>
2025-04-22 15:56:05 +0530

    basics-done

--- src/Components/Todoitems/todoitem.jsx
+++ src/Components/Todoitems/todoitem.jsx
@@ -3,20 +3,20 @@ import './todoitem.css'
 import { useState } from 'react';
 import checkbox from "../../assets/circle.png"
 import deleteicon from "../../assets/delete.png"
+import tickicon from "../../assets/tick.png"
 
-const Todoitem = ({task}) => {
+const Todoitem = ({task,id,iscompleted,deletebutton,completebutton}) => {
 
   return (
 
     <div className='todoitemdiv'>
         <div className="todoitem p-2">
-            <button onClick={(event)=>{
-                event.target.classList.toggle('toggleclass')}} className='checkboxbutton '>
-                <img src={checkbox} alt="" className='w-8' />
+            <button onClick={()=>{completebutton(id)}} className='checkboxbutton '>
+                <img src={iscompleted ? tickicon :checkbox } alt="" className='w-8' />
             </button>
-            <p className='text-xl px-1 pb-1 font-medium'>{task}</p>
+            <p className={`text-xl px-1 pb-1 font-medium ${iscompleted ? "line-through" : ""}`}>{task}</p>
             <button className='deletebutton mr-3'>
-                <img src={deleteicon} alt="" className='w-8'/>
+                <img src={deleteicon} alt="" className='w-8' onClick={()=>{deletebutton(id)}}/>
             </button>
         </div>
     </div>
--- src/Components/Todolist/Todolist.jsx
+++ src/Components/Todolist/Todolist.jsx
@@ -4,36 +4,63 @@ import { useState } from 'react'
 import { useEffect } from 'react'
 import { useRef } from 'react'
 import Todoitem from '../Todoitems/todoitem'
-import calendaricon from "../../assets/calendar.png"
-import remindericon from "../../assets/reminder.png"
-import flagicon from "../../assets/flag.png"
+
 
 const Todolist = () => {
 
   const inputtext = useRef();
+  const [todolist , settodolist] = useState(localStorage.getItem("todolist") ? JSON.parse(localStorage.getItem("todolist")) : []);
 
   function addbutton() {
     const inputvalue= inputtext.current.value.trim();
-    inputtext.current.value = "";
+    
     console.log(inputvalue)
     if (inputvalue.length === 0) {
       alert("Please enter a task")
       return;
     }
+    const todotask={
+      id:Date.now(),
+      task:inputvalue,
+      iscompleted:false,
+    };
+    settodolist([...todolist,todotask]);
+    inputtext.current.value = "";
   }
+  const deletebutton = (id) => {
+    settodolist(todolist.filter((item) => item.id !== id));
+  }
+  const completebutton = (id) => {
+    settodolist(todolist.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          iscompleted: !item.iscompleted,
+        };
+      }
+      return item;
+    }));
+  }
+  useEffect(() => {
+    localStorage.setItem("todolist", JSON.stringify(todolist));
+    console.log(todolist);
+  },[todolist]);
+
+
   return (
     <div className='tododiv'>
       <h1 className="heading text-4xl font-extrabold">To Do List</h1>
       <div className="todoform rounded-2xl shadow-xl p-2 mx-2 my-2">
         <div action="" className='flex justify-between items-center my-2 mx-2'>
           <input ref={inputtext}type="text" className='rounded-l-3xl px-4 pl-6' placeholder='Add a New Task' />
-          {/* <button><img src={flagicon} alt="" className='w-8 bg-white hoverbuttons'/></button>
-          <button><img src={calendaricon} alt="" className='w-8 bg-white hoverbuttons'/></button>
-          <button><img src={remindericon} alt="" className='w-8 bg-white hoverbuttons'/></button> */}
           <button onClick={addbutton} className='addbutton px-5 rounded-r-3xl' type='button'>Add</button>
         </div>
         <div className="entries h-4/5 my-5 rounded-3xl ml-2 mr-2 shadow-xl overflow-y-auto p-3">
-          <Todoitem task={addbutton}/>
+          {todolist.map((item , index) => {
+            return (
+              <Todoitem key={index} task={item.task} id={item.id} iscompleted={item.iscompleted} deletebutton={deletebutton} completebutton={completebutton}/>
+            )
+          })}
         </div>
       </div>
 
